5c1bcea6fae00fc157e896f4fcb1b6f2e5493649
Gray <>
2021-12-03 00:27:09 +0600

    first commit

---
+++ zxc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  zxc
+//
+//  Created by ablai erzhanov on 03.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
